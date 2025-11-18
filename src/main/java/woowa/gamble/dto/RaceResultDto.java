73703b5371cc340bfbc90b1921820a343e2cf706
gitue <gitue11@gmail.com>
2025-11-18 21:46:52 +0900

    feat: 애니메이션 기능 제거

--- src/main/java/woowa/gamble/dto/RaceResultDto.java
+++ src/main/java/woowa/gamble/dto/RaceResultDto.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 @Getter @Setter
 public class RaceResultDto {
-    private List<Map<String, Integer>> history; // 매 라운드별 자동차들의 위치 (Key: 차이름, Value: 거리)
     private List<String> winners;               // 최종 우승자 이름 목록
     private boolean isUserWin;                  // 사용자가 이겼는지 여부
     private long reward;                        // 획득한 상금
